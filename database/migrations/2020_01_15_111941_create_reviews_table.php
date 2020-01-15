cc1b2753756ae9960a56083f565d021c9a436efe
kantafujimori <fujimorikanta@fujimorikantanoMacBook-ea.local>
2020-01-15 20:54:59 +0900

    テーブル作成

---
+++ database/migrations/2020_01_15_111941_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('title');
+            $table->text('comment');
+            $table->integer('book_id')->nullable();
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
